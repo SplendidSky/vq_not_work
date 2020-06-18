2a638b071b801dc1fdd4cd10c911c27b7a4372b6
田各 <splendidsky.cwc@alibaba-inc.com>
2020-06-18 13:42:23 +0800

    1

---
+++ mem_user.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+#define MEM_SIZE 1000000
+#define ARRAY_X 1000
+#define ARRAY_Y 1000
+
+void print_mem_usage() {
+    FILE* file = fopen("/proc/self/status", "r");
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL) {
+        if (strncmp(line, "VmRSS:", 6) == 0) {
+            cout << line << endl;
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+int main() {
+    char **tmp;
+    for (int i = 0; i < ARRAY_X; i++) {
+        tmp[i] = new char[ARRAY_Y];
+        for (int j = 0; j < ARRAY_Y; j++) {
+            tmp[i][j] = (i * j) % 26 + 'A';
+        }
+        print_mem_usage();
+    }
+
+    getchar();
+    return 0;
+}
